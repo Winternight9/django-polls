da448751f9673811febd3d2b1be609ac5c4ed842
Winternight9 <poomiix9@gmail.com>
2019-11-09 17:41:39 +0700

    added auth user must login before vote features

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
@@ -22,3 +23,11 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choice_text
+
+class Vote(models.Model):
+    """A vote by a user for a particular choices in a Question"""
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice= models.ForeignKey(Choice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
